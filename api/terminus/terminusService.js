55aecec8fab751eed3f90a12e793fbb3564ba07f
bnonni <me@bryanwnonni.com>
2022-06-10 13:54:06 -0500

    fixed

--- api/terminus/terminusService.js
+++ api/terminus/terminusService.js
@@ -6,10 +6,12 @@ const bitcoiners = require('../../db/collection');
 const signup = async (info, tier, recurrence) => {
     try {
         const offerId = offerMap[tier + recurrence]
-        console.log(offerId)
-        const offer = await apiCall(`/v1/offers/listOffers?offer_id=${offerId}`, 'GET')
-        await bitcoiners.add({ info, tier, recurrence, offer: offer[0].bolt12 });
-        return { success: true, message: offer[0].bolt12 };
+        const res = await apiCall(`/v1/offers/listOffers?offer_id=${offerId}`, 'GET')
+        const data = await res.json()
+        const offer = data.offers[0].bolt12
+        console.log(offer)
+        await bitcoiners.add({ info, tier, recurrence, offer: offer });
+        return { success: true, message: offer };
     } catch (error) {
         debug.error(error.stack, error.status, error.message);
         throw new Error(error);
--- utils/apiCall.js
+++ utils/apiCall.js
@@ -1,30 +1,19 @@
 const fetch = require('node-fetch');
 const BASE_URL = process.env.BASE_URL;
 const API_KEY = process.env.API_KEY;
+const HEADERS = { 'Content-Type': 'application/json', macaroon: API_KEY };
 
 const apiCall = async (path, method, json = null) => {
     if (method == 'POST') {
-        console.log(BASE_URL + path)
         return await fetch(BASE_URL + path, {
             method: method,
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'application/json',
-                'macaroon': API_KEY,
-                'rejectUnauthorized': false
-            },
-            credentials: 'include',
+            headers: HEADERS,
             body: JSON.stringify(json),
         });
     } else {
-        console.log(BASE_URL + path)
         return await fetch(BASE_URL + path, {
             method: method,
-            headers: {
-                'Content-Type': 'application/json',
-                macaroon: API_KEY,
-                redirect: 'follow'
-            }
+            headers: HEADERS,
         });
     }
 };
