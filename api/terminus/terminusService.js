65ff23a0ce3161023e6fa3a4109c4c77607b5801
bnonni <me@bryanwnonni.com>
2022-06-10 13:47:20 -0500

    fix tls

--- api/terminus/terminusService.js
+++ api/terminus/terminusService.js
@@ -1,11 +1,12 @@
 const debug = require('../../utils/debug');
 const offerMap = require('../../utils/offerMap');
-const apiCall = require('../../utils/apiCall')
+const { apiCall } = require('../../utils/apiCall')
 const bitcoiners = require('../../db/collection');
 
 const signup = async (info, tier, recurrence) => {
     try {
         const offerId = offerMap[tier + recurrence]
+        console.log(offerId)
         const offer = await apiCall(`/v1/offers/listOffers?offer_id=${offerId}`, 'GET')
         await bitcoiners.add({ info, tier, recurrence, offer: offer[0].bolt12 });
         return { success: true, message: offer[0].bolt12 };
--- utils/apiCall.js
+++ utils/apiCall.js
@@ -3,25 +3,28 @@ const BASE_URL = process.env.BASE_URL;
 const API_KEY = process.env.API_KEY;
 
 const apiCall = async (path, method, json = null) => {
-    if (json) {
+    if (method == 'POST') {
+        console.log(BASE_URL + path)
+        return await fetch(BASE_URL + path, {
+            method: method,
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json',
+                'macaroon': API_KEY,
+                'rejectUnauthorized': false
+            },
+            credentials: 'include',
+            body: JSON.stringify(json),
+        });
+    } else {
         console.log(BASE_URL + path)
         return await fetch(BASE_URL + path, {
             method: method,
             headers: {
                 'Content-Type': 'application/json',
                 macaroon: API_KEY,
-            },
-            credentials: 'include',
-            body: JSON.stringify(json),
-        });
-    } else {
-        return await fetch(BASE_URL + path, {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-                macaroon: API_KEY,
-            },
-            credentials: 'include',
+                redirect: 'follow'
+            }
         });
     }
 };
